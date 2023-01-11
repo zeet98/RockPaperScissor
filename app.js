be50af171bbb2c1cf433c600a4b8e2ed8f422cd9
zeshan <zeshan.tawa98@gmail.com>
2023-01-11 11:30:22 -0500

    continued RPS game development

--- app.js
+++ app.js
@@ -47,3 +47,32 @@ btn.addEventListener('click', function(){
         item.addEventListener("click", playSound);
     })
 });
+
+//function to hide end div and show winner 
+function hideEndContainer() {
+    let gameContainer = document.querySelector('.gamecontainer');
+    let bottomContainer = document.getElementById('bottomcontainer');
+    gameContainer.style.opacity = 0;
+    gameContainer.style.transform = 'scale(0)';
+    //add timeout length mathcing animation 
+    gameContainer.style.display = 'none';
+    bottomContainer.style.display = 'block';
+}
+btn.addEventListener('click', function(){
+    startContainer.style.opacity = 0;
+    startContainer.style.transform = 'scale(0)';
+    //add timeout lenght matching animation duration
+    window.setTimeout(function () { 
+        startContainer.style.display = 'none';  
+      }, 700);
+      setTimeout(() => {typeWriter();}, 1000);
+      //add event listener to all of the game buttons
+      gameButtons = document.querySelectorAll(".button").forEach(item => {
+          item.addEventListener("click", playSound);
+      })
+});
+
+//global variables
+let playerScore = 0;
+let computerScore = 0;
+let gamesPlayed = 0;
